318f663059d3302543c82a85ed00ccc099f57fba
Hayk <hayk.hayr3@gmail.com>
2020-12-04 01:22:45 +0400

    Add project files.

---
+++ Less02_task01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Less02_task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 myClass = new Class1();
+            myClass.Method();
+
+            Console.ReadKey();
+        }
+
+        partial class Class1
+        {
+            partial void PartialMethod();
+        }
+
+        partial class Class1
+        {
+            partial void PartialMethod()
+            {
+                Console.WriteLine("partial method");
+            }
+
+            public void Method() { PartialMethod(); }
+        }
+    }
+}
